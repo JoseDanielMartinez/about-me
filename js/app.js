06907e23b71302e51358b5136cc5b7ce9e081297
Jose Daniel Martinez <jmartinez42528@gmail.com>
2022-11-30 14:39:50 +0000

    added new about me files

---
+++ js/app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+let userName = prompt('What is you name');
+
+alert('Welcome to my site ${userName}! Please answer the following questions with yes/no or y/n');
+
+let questionOneGuess = prompt('Do I have a daughter');
+
+if(questionOneGuess === 'yes' || questionOneGuess === 'y'){
+  alert
+}
