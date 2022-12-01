1e8a3f76996534aa874f5e196ae41a14ce83e778
Jose Daniel Martinez <jmartinez42528@gmail.com>
2022-12-01 09:59:39 +0000

    tried to figure out homework

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# about-me
+General project information 
+  Lab 03
+Instructions for installation and use
+  The link for my live site is:
+    XX
+Information on the license
+  See MIT License file
+Acknowledgements, etc.
+  Source: [Dani Krossing; YouTube; How to Always Keep the Footer at the Bottom of A Page | Learn HTML and CSS | HTML Tutorial](https://www.youtube.com/watch?v=qlCIXXhSX6Y)
--- js/app.js
+++ js/app.js
@@ -1,50 +1,50 @@
 'use strict';
 
-console.log('Hey world hey');
+// console.log('Hey world hey');
 
-let userName = prompt('What is your name?');
+// let userName = prompt('What is your name?');
 
-alert(`Welcome to my site ${userName}! Pop quiz time! Please answer the following questions with a yes/no or y/n`);
+// alert(`Welcome to my site ${userName}! Pop quiz time! Please answer the following questions with a yes/no or y/n`);
 
-let questionOneGuess = prompt('Do I live in Miami, FL?').toLowerCase();
+// let questionOneGuess = prompt('Do I live in Miami, FL?').toLowerCase();
 
-if(questionOneGuess === 'yes' || questionOneGuess ==='y'){
-  alert('Great work!');
-} else if(questionOneGuess === 'no' || questionOneGuess === 'n'){
-  alert('No bueno amigo...you got it wrong');
-}
+// if(questionOneGuess === 'yes' || questionOneGuess ==='y'){
+//   alert('Great work!');
+// } else if(questionOneGuess === 'no' || questionOneGuess === 'n'){
+//   alert('No bueno amigo...you got it wrong');
+// }
 
-let questionTwoGuess = prompt('Was I in the military?').toLowerCase();
+// let questionTwoGuess = prompt('Was I in the military?').toLowerCase();
 
-if(questionTwoGuess === 'yes' || questionTwoGuess ==='y'){
-  alert('Awesomesauce!');
-} else if(questionTwoGuess === 'no' || questionTwoGuess === 'n'){
-  alert('*cue fail music');
-}
+// if(questionTwoGuess === 'yes' || questionTwoGuess ==='y'){
+//   alert('Awesomesauce!');
+// } else if(questionTwoGuess === 'no' || questionTwoGuess === 'n'){
+//   alert('*cue fail music');
+// }
 
-let questionThreeGuess = prompt('Do I have a tech background?').toLowerCase();
+// let questionThreeGuess = prompt('Do I have a tech background?').toLowerCase();
 
-if(questionThreeGuess === 'yes' || questionThreeGuess ==='y'){
-  alert('Correctomundo!');
-} else if(questionThreeGuess === 'no' || questionThreeGuess === 'n'){
-  alert('So close!');
-}
+// if(questionThreeGuess === 'yes' || questionThreeGuess ==='y'){
+//   alert('Correctomundo!');
+// } else if(questionThreeGuess === 'no' || questionThreeGuess === 'n'){
+//   alert('So close!');
+// }
 
-let questionFourGuess = prompt('Do I joke in the class Zoom chats?').toLowerCase();
+// let questionFourGuess = prompt('Do I joke in the class Zoom chats?').toLowerCase();
 
-if(questionFourGuess === 'yes' || questionFourGuess ==='y'){
-  alert('For sure! Keeps things light!');
-} else if(questionFourGuess === 'no' || questionFourGuess === 'n'){
-  alert('Maybe you should read the chats more often, lol!');
-}
+// if(questionFourGuess === 'yes' || questionFourGuess ==='y'){
+//   alert('For sure! Keeps things light!');
+// } else if(questionFourGuess === 'no' || questionFourGuess === 'n'){
+//   alert('Maybe you should read the chats more often, lol!');
+// }
 
-let questionFiveGuess = prompt('Is Audrey my favorite Code Fellows instructor?').toLowerCase();
+// let questionFiveGuess = prompt('Is Audrey my favorite Code Fellows instructor?').toLowerCase();
 
-if(questionFiveGuess === 'yes' || questionFiveGuess ==='y'){
-  alert('You know it!');
-} else if(questionFiveGuess === 'no' || questionFiveGuess === 'n'){
-  alert('Do not you put that evil on me Ricky Bobby!');
-}
+// if(questionFiveGuess === 'yes' || questionFiveGuess ==='y'){
+//   alert('You know it!');
+// } else if(questionFiveGuess === 'no' || questionFiveGuess === 'n'){
+//   alert('Do not you put that evil on me Ricky Bobby!');
+// }
 
-alert(`Pop quiz over ${userName}. Take a look at my bio and I will see you in class.  Take care!`);
+// alert(`Pop quiz over ${userName}. Take a look at my bio and I will see you in class.  Take care!`);
 
