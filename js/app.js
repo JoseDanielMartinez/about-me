1844deb3f2182a67ac239def921ed7a3f69b3e4a
Jose Daniel Martinez <jmartinez42528@gmail.com>
2022-12-01 20:38:37 +0000

    updated files after remo help ticket

--- js/app.js
+++ js/app.js
@@ -8,43 +8,79 @@
 
 // let questionOneGuess = prompt('Do I live in Miami, FL?').toLowerCase();
 
-// if(questionOneGuess === 'yes' || questionOneGuess ==='y'){
+// if (questionOneGuess === 'yes' || questionOneGuess === 'y') {
 //   alert('Great work!');
-// } else if(questionOneGuess === 'no' || questionOneGuess === 'n'){
+// } else if (questionOneGuess === 'no' || questionOneGuess === 'n') {
 //   alert('No bueno amigo...you got it wrong');
 // }
 
 // let questionTwoGuess = prompt('Was I in the military?').toLowerCase();
 
-// if(questionTwoGuess === 'yes' || questionTwoGuess ==='y'){
+// if (questionTwoGuess === 'yes' || questionTwoGuess === 'y') {
 //   alert('Awesomesauce!');
-// } else if(questionTwoGuess === 'no' || questionTwoGuess === 'n'){
+// } else if (questionTwoGuess === 'no' || questionTwoGuess === 'n') {
 //   alert('*cue fail music');
 // }
 
 // let questionThreeGuess = prompt('Do I have a tech background?').toLowerCase();
 
-// if(questionThreeGuess === 'yes' || questionThreeGuess ==='y'){
+// if (questionThreeGuess === 'yes' || questionThreeGuess === 'y') {
 //   alert('Correctomundo!');
-// } else if(questionThreeGuess === 'no' || questionThreeGuess === 'n'){
+// } else if (questionThreeGuess === 'no' || questionThreeGuess === 'n') {
 //   alert('So close!');
 // }
 
 // let questionFourGuess = prompt('Do I joke in the class Zoom chats?').toLowerCase();
 
-// if(questionFourGuess === 'yes' || questionFourGuess ==='y'){
+// if (questionFourGuess === 'yes' || questionFourGuess === 'y') {
 //   alert('For sure! Keeps things light!');
-// } else if(questionFourGuess === 'no' || questionFourGuess === 'n'){
+// } else if (questionFourGuess === 'no' || questionFourGuess === 'n') {
 //   alert('Maybe you should read the chats more often, lol!');
 // }
 
 // let questionFiveGuess = prompt('Is Audrey my favorite Code Fellows instructor?').toLowerCase();
 
-// if(questionFiveGuess === 'yes' || questionFiveGuess ==='y'){
+// if (questionFiveGuess === 'yes' || questionFiveGuess === 'y') {
 //   alert('You know it!');
-// } else if(questionFiveGuess === 'no' || questionFiveGuess === 'n'){
+// } else if (questionFiveGuess === 'no' || questionFiveGuess === 'n') {
 //   alert('Do not you put that evil on me Ricky Bobby!');
 // }
 
-// alert(`Pop quiz over ${userName}. Take a look at my bio and I will see you in class.  Take care!`);
+// alert(`Pop quiz over ${userName}. Next, let's play a guessing game!`);
+
+// TODO:add sixth question that takes in a numeric input by prompting 
+
+// TODO: the user to guess a number
+
+// TODO: Indicate thru an alert if the guess is too high or too low
+
+// TODO: it should give the user exactly four opportunities to get the correct answer
+
+// TODO: after all attempts have been exhausted tell the user the correct answer
+
+let myNum = 25;
+
+for (let i = 0; i < 4; i++) {
+  let questionSixGuess = prompt('Guess a number from 1-100');
+
+  if (questionSixGuess > myNum) {
+    alert('Just a bit over the mark!');
+  }
+
+  if (questionSixGuess < myNum) {
+    alert('Too low!');
+  }
+
+  if (Number(questionSixGuess) === myNum) {
+    alert('You\'re right on the mark!');
+    break;
+  }
+
+  if (i === 3){
+    alert('You are done! The answer was 25');
+  }
+}
+
+
+
 
